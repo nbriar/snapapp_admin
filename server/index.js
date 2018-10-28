8d0f80ba0ded4b840bbd82984276c31fd48e3720
Nathan Briar Davis <nbriardavis@gmail.com>
2018-10-27 22:22:07 -0600

    adds host back in

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
+const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 8080
 const logger = require('morgan')
 
@@ -27,9 +28,9 @@ async function start() {
   app.use(nuxt.render)
 
   // Listen the server
-  app.listen(port)
+  app.listen(port, host)
   consola.ready({
-    message: `Server listening on port:${port}`,
+    message: `Server listening on http://${host}:${port}`,
     badge: true
   })
 }

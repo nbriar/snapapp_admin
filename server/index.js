c30776d2a021b4abc2b98d3aaa6bb46f5c200433
Nathan Briar Davis <nbriardavis@gmail.com>
2018-10-27 22:08:17 -0600

    remove host

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
-const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 8080
 const logger = require('morgan')
 
@@ -28,9 +27,9 @@ async function start() {
   app.use(nuxt.render)
 
   // Listen the server
-  app.listen(port, host)
+  app.listen(port)
   consola.ready({
-    message: `Server listening on http://${host}:${port}`,
+    message: `Server listening on port:${port}`,
     badge: true
   })
 }

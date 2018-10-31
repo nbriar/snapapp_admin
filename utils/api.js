d14835cd34f82d1d81d63066199c2410ab18c323
Nathan Briar Davis <nbriardavis@gmail.com>
2018-10-30 23:02:09 -0600

    adds comment

--- utils/api.js
+++ utils/api.js
@@ -5,13 +5,16 @@ export default function ({token, query, variables, callback}) {
   http(token).post(config.SNAPAPP_API_URL, {query: query, variables: variables})
     .then(response => {
       const data = response.data.data
+
       if (response.data.errors) {
         console.log('ERROR: ', response.data.errors)
         return null
       }
+
       callback(data)
     })
     .catch(e => {
+      // TODO probably need to do a bugsnag here
       console.log('ERROR: ', e)
       return null
     })
